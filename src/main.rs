c372e4571ea459450c906de3bbc66024e6d24a8b
Daniel Vorhauer <daniel@hexerei.net>
2023-01-14 20:18:10 +0100

    rocket config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
 #[macro_use]
 extern crate rocket;
 
-fn main() {
-    println!("Hello, world!");
+use rocket::{Build, Rocket};
+
+#[derive(FromForm)]
+struct Filters {
+    age: u8,
+    active: bool,
 }
+
+#[get("/user/<uuid>", rank=1, format="text/plain")]
+fn user(uuid: &str) {
+    unimplemented!()
+}
+
+#[get("/users/<grade>?<filters..>")]
+fn users(grade: u8, filters: Filters) {
+    unimplemented!()
+}
+
+// #[get("/")]
+// async fn index() -> &'static str {
+//     "Hello, Rocket!"
+// }
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/", routes![user, users])
+}
+
+// #[rocket::main]
+// async fn main() {
+//     rocket::build()
+//         .mount("/", routes![index])
+//         .launch()
+//         .await;
+// }
