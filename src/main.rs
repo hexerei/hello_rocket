bbcf176210fcc692bc44862ffe62507ba9b67bad
Daniel Vorhauer <daniel@hexerei.net>
2023-01-15 14:53:29 +0100

    post data

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::{Build, Rocket};
+use rocket::{Build, Rocket, form::Form};
 
 #[derive(FromForm)]
 struct Filters {
     age: u8,
     active: bool,
+}
+
+#[derive(Debug)]
+struct User {}
+
+#[post("/post", data="<data>")]
+fn post(data: Form<Filters>) -> &'static str {
+    "POST Request"
 }
 
 #[get("/user/<uuid>", rank=1, format="text/plain")]
@@ -19,15 +27,26 @@ fn users(grade: u8, filters: Filters) {
     unimplemented!()
 }
 
-// #[get("/")]
-// async fn index() -> &'static str {
-//     "Hello, Rocket!"
-// }
+#[get("/<rank>", rank=1)]
+fn first(rank: u8) -> String {
+    let result = rank + 10;
+    format!("Your rank is, {}!", result)
+}
+
+#[get("/<name>", rank=2)]
+fn second(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello, Rocket!"
+}
 
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build()
-        .mount("/", routes![user, users])
+        .mount("/", routes![post])
 }
 
 // #[rocket::main]
